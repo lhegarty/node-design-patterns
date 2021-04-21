cad478cd98737d90e1ebc2f31c1c13e9d9e1f4c1
lukehegarty <ldhegarty@gmail.com>
2021-04-21 12:06:11 +0100

    update errors

--- chapter 2/errors.js
+++ chapter 2/errors.js
@@ -15,18 +15,45 @@ fs.readFile('data.txt', 'utf-8', function() {
 
 // to propagate errors, they need to be passed to the next callback in the chain
 
+// The reason this works is because the callback we pass to fs.readFile is async and invoked by the event loop after the syncronous
+// intial code has been run
 function readMyFile(filename, callback) {
   fs.readFile(filename, 'utf-8', function(err, data) {
     let parsed;
     if (err) {
       return callback(err)
-
-      try {
-        parsed = JSON.parse(data);
-      } catch {
-        return callback(err);
-      }
-      callback(null, parsed);
     }
+
+    try {
+      parsed = JSON.parse(data);
+    } catch {
+      return callback(err);
+    }
+    callback(null, parsed);
   });
 }
+
+
+// CODE RUN SYNCRONOUSLY
+function readMyFile(filename, callback) {
+  fs.readFile(filename, 'utf-8', function(err, data) {
+  });
+}
+
+// CODE RUN AFTERWARDS - ASYNCRONOUSLY
+
+let parsed;
+if (err) {
+  return callback(err)
+}
+
+try {
+  parsed = JSON.parse(data);
+} catch {
+  return callback(err);
+}
+callback(null, parsed);
+
+
+// IF WE DIDN'T PUT THE TRY - CATCH IN THE ASYNC BLOCK, IT WOULDN'T CATCH ANY ERRORS COMING FROM JSON.PARSE AS IT WOULD HAVE BEEN RUN ON
+// THE SYNCRONOUS FIRST PASS INSTEAD.
